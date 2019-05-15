e30849ad3770fe8e19dc85b29775bc806b4278dc
boa <boa.matule@gmail.com>
2019-05-15 15:36:51 +0200

    setting up 'The controller'; Test failing: ForbiddenAttributesError.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
+      resources :performance_data, only: [:create]
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
