d240c9f899da2b8166b2b66d1fd4770a8d37bc17
boa <boa.matule@gmail.com>
2019-05-15 13:22:50 +0200

    Adding a User class done sucessfully.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  #devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   namespace :api do
     namespace :v0 do
-      resources :pings, only: [:index], constraints: { format: 'json' }
+      namespace :v1 do
+        resources :pings, only: [:index], constraints: { format: 'json' }
+      end
     end
   end
 end
