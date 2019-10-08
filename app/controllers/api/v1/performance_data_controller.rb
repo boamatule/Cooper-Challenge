f7de63ef65695e885dcf638b325d99e76b6a2204
Boa Matule <boa.matule@gmail.com>
2019-10-08 13:57:23 +0200

    Small errors fix

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::PerformanceDataController < ApplicationController
   before_action :authenticate_api_v1_user!
 
+  def index 
+    @collection = current_api_v1_user.performance_data
+    render json: { entries: @collection }
+  end
+  
   def create
     @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
 
@@ -9,11 +14,6 @@ class Api::V1::PerformanceDataController < ApplicationController
     else
       render json: { error: @data.errors.full_messages }
     end
-  end
-
-  def index 
-    @collection = current_api_v1_user.performance_data
-    render json: { entries: @collection }
   end
 
   private
